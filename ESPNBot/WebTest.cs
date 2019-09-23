e01c47e836004102f93857c06f5380c93a987f3e
Jeff <jeffjewett27@gmail.com>
2019-09-23 00:52:18 -0400

    Added test that can open login form

--- ESPNBot/WebTest.cs
+++ ESPNBot/WebTest.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using SeleniumExtras.WaitHelpers;
 using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,7 @@ using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using System.Threading;
+using OpenQA.Selenium.Support.UI;
 
 namespace ESPNBot
 {
@@ -94,6 +97,35 @@ namespace ESPNBot
 
             var elements = driver.FindElements(By.XPath("//tr//td//div[@title='Opponent']"));
             //Table2__tr Table2__tr--lg Table2__odd
+        }
+
+        [Test]
+        public void TestLogin()
+        {
+            driver.Url = "https://fantasy.espn.com/football/team?leagueId=61483480&teamId=10&seasonId=2019";
+
+            Thread.Sleep(2000);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(4));
+            var profileElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(
+                By.XPath("//*[@id='espn-analytics']/div/div[2]/div/div/nav/ul[3]/li[2]/div[1]")));
+            Actions action = new Actions(driver);
+            action.MoveToElement(profileElement).Perform();
+            Thread.Sleep(100);
+            action.MoveByOffset(0, 200).MoveByOffset(0, 0).Perform();
+            Thread.Sleep(200);
+            var loginElement = driver.FindElement(By.XPath("//*[@id='espn-analytics']/div/div[2]/div/div/nav/ul[3]/li[2]/div[2]/div/div/ul/li[3]"));
+            action.MoveToElement(loginElement).Perform();
+            Thread.Sleep(100);
+            action.Click(loginElement).Perform();
+
+            /*Thread.Sleep(2000);
+            //var usernameElement = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(
+            //By.XPath("//*[@id='did-ui-view']/div/section/section/form/section/div[1]/div/label/span[2]/input")));
+            var usernameElement = driver.FindElement(By.XPath("//*[@id='did-ui-view']/div/section/section/form/section/div[1]/div/label/span[2]/input"));
+            ////*[@id="did-ui-view"]/div/section/section/form/section/div[1]/div/label/span[2]/input
+            action.SendKeys(usernameElement, "jjewett@bcsmail.org").Perform();
+
+            Thread.Sleep(2000);*/
         }
 
         [TearDown]
